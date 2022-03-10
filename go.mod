29df7fea364ce1878ef3a4cb93278c9d5bd0e2e9
SodaCris <18463922396@163.com>
2022-03-10 10:49:38 +0800

    添加磁盘函数和测试

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/soda92/DiskTool
 
 go 1.17
+
+require (
+	github.com/shirou/gopsutil v3.21.11+incompatible
+	gopkg.in/ini.v1 v1.66.4
+)
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
+)
--- hello.go
+++ hello.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/shirou/gopsutil/disk"
+	"gopkg.in/ini.v1"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func read_config() (string, string, error) {
+	cfg, err := ini.Load("D:/LT_WXCL_Dll/LT_WXCLCFG.ini")
+	if err != nil {
+		return "", "", err
 	}
+	number := cfg.Section("LT_WXCLCFG").Key("TrainNum").String()
+	device := cfg.Section("LT_WXCLCFG").Key("HDD").String()
+	return device, number, nil
+}
+
+func disk_usage(device string) (float64, error) {
+	_, err := os.Stat(device)
+	if err != nil {
+		return 0, err
+	}
+	s, _ := disk.Usage(device)
+	return s.UsedPercent, nil
 }
 
 func main() {
-	d1 := []byte("hello\ngo\n")
-	err := os.WriteFile("D:/data1.txt", d1, 0644)
-	check(err)
+	_, device, err := read_config()
+	if err != nil {
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
+
+	percent, err := disk_usage(device)
+
+	if err != nil {
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
+
+	if percent < 0.01 {
+		fmt.Printf("Disk is full")
+	} else {
+		fmt.Printf("Disk not full")
+	}
+
 }
